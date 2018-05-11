f0ec4e17488bcee369290a15910faa77e01516b6
Pablo Satler <pablosatler@gmail.com>
2018-05-11 16:04:59 -0300

    Add code to display books currently on shelves on the main page

--- src/App.js
+++ src/App.js
@@ -7,20 +7,36 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    wantToRead: [],
+    currentlyReading: [],
+    read: []
   }
 
   componentDidMount(){
-    BooksAPI.getAll().then((books) => { //getting book list on shelves from server
+    BooksAPI.getAll().then((books) => { //getting books list on shelves from server
+      books.map( (book) => {
+          if(book.shelf === "currentlyReading"){
+            this.setState( { currentlyReading: [...this.state.currentlyReading, book] } )
+          }
+          else if (book.shelf === "wantToRead"){
+            this.setState( { wantToRead: [...this.state.wantToRead, book] } )
+          }
+          else if (book.shelf === "read"){
+            this.setState( { read: [...this.state.read, book] } )
+          }
+      })
       this.setState({ books:books })
     })
+
+
   }
-
-
-
 
   render() {
     console.log('Books', this.state.books);
+    console.log('currentlyReading', this.state.currentlyReading)
+    console.log('wantToRead', this.state.wantToRead)
+    console.log('read', this.state.read)
 
     return (
       <div className="app">
@@ -32,7 +48,12 @@ class BooksApp extends React.Component {
         )}/>
 
         <Route exact path="/" render={() => (
-          <Bookshelves />
+          <Bookshelves
+            listOfBooksOnShelves={this.state.books}
+            listOfBookWantToRead={this.state.wantToRead}
+            listOfBookCurrentlyReading={this.state.currentlyReading}
+            listOfBookRead={this.state.read}
+          />
         )}/>
 
       </div>
