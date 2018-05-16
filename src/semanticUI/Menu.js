0366f1a7305c97a44de8ff9e5d573c5a8e3b2ad5
Pablo Satler <pablosatler@gmail.com>
2018-05-15 21:40:23 -0300

    feat: Add some tweaks to menu code

--- src/semanticUI/Menu.js
+++ src/semanticUI/Menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
 import '../App.css'
 import logo from '../icons/reactLogo.svg'
 
@@ -7,6 +8,10 @@ export default class MenuExampleStackable extends Component {
   state = { activeItem: 'home' }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  goToGitHub = () => {
+    console.log('GITHUB')
+  }
 
   render() {
     const { activeItem } = this.state
@@ -16,14 +21,20 @@ export default class MenuExampleStackable extends Component {
         <Menu.Item>
           <img src={logo} className="App-logo" alt="logo" />
         </Menu.Item>
-        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-        <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+        <Link exact to="/">
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        </Link>
+        <Link to="/search">
+          <Menu.Item name='search'
+            active={activeItem === 'search'} onClick={this.handleItemClick} />
+        </Link>
+        {/*tried to use Link here as well, though without success */}
         <Menu.Item className='right' icon='code' active={activeItem === 'code'}
-          onClick={this.handleItemClick}>
-          {/* <Icon name='code' /> */}
+            onClick={this.goToGitHub} >
+            <a target="_blank" href="https://github.com/psatler/myReads_React">
+              <Icon name='code' />
+            </a>
         </Menu.Item>
-
       </Menu>
     )
   }
