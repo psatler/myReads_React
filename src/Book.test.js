b6fc66e08e8dd08bce0d3d781f60a6a6f268473b
Pablo Satler <pablosatler@gmail.com>
2018-05-16 23:12:56 -0300

    test: Add dev dependency to deal with local storage mocks

--- src/Book.test.js
+++ src/Book.test.js
@@ -6,19 +6,17 @@ import Book from './Book'
 
 describe('[Component] Book', () => {
 
-  // global.localStorage = {
-  //   token: jest.fn(),
-  // };
-  //
-  // console.log('Aqui');
 
-  it('should be able to run tests', () => {
-        expect(1 + 2).toEqual(3);
-  });
-  // it('displays book if information is present', () => {
-  //   const wrapper = shallow(
-  //     <Book />
-  //   );
-  //   expect(wrapper.contains(children)).toEqual(true);
+
+  console.log('Aqui');
+
+  // it('should be able to run tests', () => {
+  //       expect(1 + 2).toEqual(3);
   // });
+  it('displays book if information is present', () => {
+    const wrapper = shallow(
+      <Book />
+    );
+    expect(wrapper.contains(children)).toEqual(true);
+  });
 });
